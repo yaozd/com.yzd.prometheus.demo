912365695dabea88c6348e73728ffec067053ca7
yzd <yzdahlma@gmail.com>
2020-03-17 11:59:32 +0800

    匹配所有路由

--- com.yzd.prometheus.demo/src/main/java/com/yzd/prometheus/demo/controller/api/HomeControllerApi.java
+++ com.yzd.prometheus.demo/src/main/java/com/yzd/prometheus/demo/controller/api/HomeControllerApi.java
@@ -3,24 +3,37 @@ package com.yzd.prometheus.demo.controller.api;
 import com.yzd.prometheus.demo.config.GitInfo;
 import com.yzd.prometheus.demo.config.MetricsService;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 @RestController
 public class HomeControllerApi {
+    private final static Logger logger = LoggerFactory.getLogger(HomeControllerApi.class);
     @Autowired
     MetricsService metricsService;
 
-    @RequestMapping("/")
-    public String index() {
-        metricsService.sendSuccessIncrement();
-        return "this is prometheus demo";
+    /**
+     * 匹配所有路由
+     * @param request
+     * @return
+     */
+    @RequestMapping("/**")
+    public String index(HttpServletRequest request) {
+        String msg = "request uri:" + request.getRequestURI();
+        logger.info(msg);
+        return msg;
     }
 
     @RequestMapping("/greeting")
     public String greeting() {
+        metricsService.sendSuccessIncrement();
         return "greeting";
     }
 
